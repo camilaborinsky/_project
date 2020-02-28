9212882a3bbe5023d397b968822d2fe54bb1578b
camilaborinsky <cborinsky@itba.edu.ar>
2020-02-28 14:03:11 -0300

    video 16 stateful widgets

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,15 @@ void main() => runApp(MaterialApp(
   home: NinjaCard(),
 ));
 
-class NinjaCard extends StatelessWidget {
+class NinjaCard extends StatefulWidget {
+  @override
+  _NinjaCardState createState() => _NinjaCardState();
+}
+
+class _NinjaCardState extends State<NinjaCard> {
+
+  //here we can declare data that will change over time
+  int ninjaLevel =0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -14,6 +22,16 @@ class NinjaCard extends StatelessWidget {
         centerTitle: true,
         backgroundColor: Colors.grey[850],
         elevation: 0.0,
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          setState(() {   //triggers the build function to run updating new data
+            ninjaLevel++;
+          });
+        },
+        child: Icon(Icons.add),
+        backgroundColor: Colors.grey[850],
+        elevation:0.0,
       ),
       body: Padding(
         padding: EdgeInsets.fromLTRB(30.0, 40.0, 30.0, 0),
@@ -57,7 +75,7 @@ class NinjaCard extends StatelessWidget {
             ),
             SizedBox(height: 10.0),
             Text(
-              '8',
+              '${ninjaLevel}',
               style: TextStyle(
                 color: Colors.amberAccent[200],
                 letterSpacing: 2.0,
